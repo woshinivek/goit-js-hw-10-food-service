1bed4dde99038f2e1ac0b7d9117a7c459fdb9fe1
woshinivek <woshinivek@yahoo.com>
2022-08-15 22:34:54 -0400

    dark theme working, but stilll nedd doing something

--- src/index.js
+++ src/index.js
@@ -1,11 +1,36 @@
-document.body.classList.add('light-theme');
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
 
 const switchThemeToggle = document.querySelector('#theme-switch-toggle');
 
 switchThemeToggle.addEventListener('change', onToggleChange);
 
-function onToggleChange(evt) {
-  document.body.classList.remove('light-theme');
+document.body.classList.add(Theme.LIGHT);
 
-  document.body.classList.add('dark-theme');
+// localStorage.setItem('theme', Theme.LIGHT);
+// document.body.classList.add(localStorage.getItem('theme'));
+
+// console.log(localStorage);
+
+function onToggleChange(evt) {
+  const isThemeDark = document.body.classList.contains(Theme.DARK);
+  const isThemeLight = document.body.classList.contains(Theme.LIGHT);
+  console.log('isThemeDark', isThemeDark);
+
+  if (isThemeDark) {
+    document.body.classList.replace(Theme.DARK, Theme.LIGHT);
+
+    return;
+  }
+
+  if (isThemeLight) {
+    document.body.classList.replace(Theme.LIGHT, Theme.DARK);
+    localStorage.setItem('theme', Theme.DARK);
+
+    return;
+  }
 }
+
+console.log(localStorage);
