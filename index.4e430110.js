94d0fcb138a1faf91583d314c5b7f4ed95c53655
woshinivek <73965613+woshinivek@users.noreply.github.com>
2022-08-16 21:06:30 +0000

    Deploying to gh-pages from @ woshinivek/goit-js-hw-10-food-service@d81f815174601ab2361682f7cd4664c35ff13f49 🚀

--- index.4e430110.js
+++
@@ -1,2 +0,0 @@
-const e="light-theme",t="dark-theme";document.querySelector("#theme-switch-toggle").addEventListener("change",(function(o){const c=document.body.classList.contains(t),s=document.body.classList.contains(e);if(console.log("isThemeDark",c),c)return void document.body.classList.replace(t,e);if(s)return document.body.classList.replace(e,t),void localStorage.setItem("theme",t)})),document.body.classList.add(e),console.log(localStorage);
-//# sourceMappingURL=index.4e430110.js.map
